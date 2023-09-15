1bf8a698d20a12fc9304d3985e9a23db189e0604
Mateo <mateo.sanabria.a@gmial.com>
2023-09-15 07:29:29 -0500

    Minima cantidad de operaciones: Divide&Conquer

--- DynamicPrograming/MinOps/minops.py
+++ DynamicPrograming/MinOps/minops.py
@@ -4,10 +4,10 @@ from sys import stdin, stdout
 
 
 def min_operation_DC(w1, w2, pointer1, pointer2):
-    if pointer1 == 0:
-        result = pointer2 + 2
-    elif pointer2 == 0:
-        result = pointer1 + 2
+    if pointer1 == -1:
+        result = pointer2 + 1
+    elif pointer2 == -1:
+        result = pointer1 + 1
     else:
         if w1[pointer1] == w2[pointer2]:
             result = min_operation_DC(w1, w2, pointer1 - 1, pointer2 - 1)
@@ -20,15 +20,19 @@ def min_operation_DC(w1, w2, pointer1, pointer2):
 
 def main():
     number = int(stdin.readline().strip())
-    original_number = number
+    current_number = number
     while number > 0:
         current_len = int(stdin.readline().strip())
-        current_len -= 1
+        #  table = [[-1 for i in range(current_len + 1)]
+        #  for j in range(current_len + 1)]
+        #  current_len -= 1
         w1 = [''] + stdin.readline().split()
         w2 = [''] + stdin.readline().split()
         result = min_operation_DC(w1, w2, current_len, current_len)
-        current_case = original_number - number + 1
-        print(f'Caso-{current_case}: {result}')
+        #  result = min_operation_DP(w1, w2, current_len, current_len,
+        #  table)
+        case_number = case_number - number + 1
+        print(f'Caso-{case_number}: {result}')
         number -= 1
 
 
